54c64b42b56bc80fdc05459b9647e7641fb410ea
Radek Werra <rwerra@intercars.eu>
2025-01-03 14:11:53 +0100

    added comments to arguments for checkout.sh script

--- checkout.sh
+++ checkout.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#$1 - url to git repo
+#$2 - directory with repo files (created after download)
+#$3 - branch to download files
+#$4 - directory or file to download
 git clone --no-checkout "$1"
 cd "$2"
 git sparse-checkout init --cone
